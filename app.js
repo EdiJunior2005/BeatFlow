25ded1be87013a396fcfebde1987cdeb8687651b
EdiJunior2005 <edicfjunior05@gmail.com>
2024-10-01 17:05:05 -0300

    final com rotas certas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config()
 conect_mongoose()
 
 app.use(express.json())
-app.use("/protegido", userRoutes)
+app.use("/", userRoutes)
 app.use("/musicas", musicaRoutes)
 app.use("/playlists", playlistRoutes)
 
